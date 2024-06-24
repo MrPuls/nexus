1f4d5334d31f9130d3fa152f0efa2ae096103746
Artem <impulsar007@gmail.com>
2024-06-24 19:08:49 +0300

    feat: added project creation form

--- src/components/projects/CreateProjectModal.vue
+++ src/components/projects/CreateProjectModal.vue
@@ -1,13 +1,79 @@
 <script setup>
+import { ref } from 'vue';
+
+const name = ref('');
+const description = ref('');
+
+function submitForm () {
+  console.log('Form submitted:', { name: name.value, email: description.value });
+  // Add your form submission logic here
+  // Reset form fields
+  name.value = '';
+  description.value = '';
+}
+
+function clearForm() {
+  // Reset form fields
+  name.value = '';
+  description.value = '';
+}
 
 </script>
 
 <template>
   <div>
-    <h1>Create Project Modal</h1>
+    <!-- Button to open the modal -->
+    <label
+      for="my-modal"
+      class="btn btn-primary"
+    >Create project</label>
+
+    <!-- Modal -->
+    <input
+      id="my-modal"
+      type="checkbox"
+      class="modal-toggle"
+    >
+    <div class="modal">
+      <div class="modal-box">
+        <h3 class="font-bold text-lg">
+          Enter Project Details
+        </h3>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Project's Name</span>
+          </label>
+          <input
+            v-model="name"
+            type="text"
+            placeholder="project name"
+            class="input input-bordered"
+          >
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Description (optional)</span>
+          </label>
+          <textarea
+            v-model="description"
+            type="text"
+            placeholder="Your project description"
+            class="textarea textarea-bordered"
+          />
+        </div>
+        <div class="modal-action">
+          <label
+            for="my-modal"
+            class="btn"
+            @click="submitForm"
+          >Submit</label>
+          <label
+            for="my-modal"
+            class="btn"
+            @click="clearForm"
+          >Close</label>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
