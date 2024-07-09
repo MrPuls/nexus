1e7f7900af7e6d688cf2ffaac367b650bc47ef01
Artem <impulsar007@gmail.com>
2024-07-10 00:52:51 +0300

    feat: fixed typo

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -18,7 +18,7 @@ export default {
     async getProjects() {
         const response = await fetch(`${BASE_URL}/projects`);
         if (!response.ok) {
-            throw new Error('Failed to fetch project');
+            throw new Error('Failed to fetch projects');
         }
         return response.json();
     },
